3a11781ac43557cde9217e6b2569e52f1e45aa0d
Sohag Hasan <58013666+mdsohaghasan@users.noreply.github.com>
2022-05-10 11:38:03 +0600

    font

--- MernStack-Client/src/Components/About/About.js
+++ MernStack-Client/src/Components/About/About.js
@@ -1,14 +1,111 @@
 import React from 'react'
+import './About.css';
+import img from './img/sohag.png'
 
 function About() {
   return (
-    <div>
-      <div>
-        <h1>Hi, I Am Sohag Hasan</h1>
-        <h2>Front End developer</h2>
+    <div class="container">
+      <div class="row">
+        <div class="col m-5">
+          <div class="intro">
+            <h3>Hi, I'm</h3>
+            <h1>Sohag Hasan</h1>
+            <h3>FrontEnd developer</h3>
+            <p>Hello! I'm Sohag Hasan a passionate software engineer. I develop web applications, mobile
+              applications, and desktop applications. My core skill is based on JavaScript and I love to do
+              most of the things using JavaScript. I love to make the web more open to the world. I have
+              graduated with a bachelor's degree in Computer Science Engineering from Chandigarh University at
+              Punjab, India in 2020. I am available for any kind of job opportunity that suits my interests.
+            </p>
+            <button class="btn btn-primary">Click Me</button>
+          </div>
+
+        </div>
+
+        <div class="col m-5">
+          <div class="max-width:50% height:auto">
+            <img src={img} class="img-fluid rounded" alt="sohag-photo" />
+          </div>
+
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col m-5">
+          <h2>Skills</h2>
+
+          <div class="bg-info pt-2 pb-1 px-1 m-2">
+            <p class="ms-3">html</p>
+            <div class="progress m-3" style={{ height: '30px' }}>
+              <div class="progress-bar bg-success" role="progressbar" style={{ width: "%25" }} aria-valuenow="25"
+                aria-valuemin="0" aria-valuemax="100">25%</div>
+            </div>
+          </div>
+
+          <div class="bg-info pt-2 pb-1 px-1 m-2">
+            <p class="ms-3">html</p>
+            <div class="progress m-3" style={{ height: '30px' }}>
+              <div class="progress-bar bg-success" role="progressbar" style={{ width: "%25" }} aria-valuenow="25"
+                aria-valuemin="0" aria-valuemax="100">25%</div>
+            </div>
+          </div>
+
+          <div class="bg-info pt-2 pb-1 px-1 m-2">
+            <p class="ms-3">html</p>
+            <div class="progress m-3" style={{ height: '30px' }}>
+              <div class="progress-bar bg-success" role="progressbar" style={{ width: "%25" }} aria-valuenow="25"
+                aria-valuemin="0" aria-valuemax="100">25%</div>
+            </div>
+          </div>
+
+        </div>
+        <div class="col">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, molestiae?
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-4">
+          <div class="border border-primary border-3 rounded m-3 p-3">
+            <i class="bi bi-apple text-primary"></i>
+            <p>Apple</p>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="border border-primary border-3 rounded m-3 p-3">
+            <i class="bi bi-apple text-primary"></i>
+            <p>Apple</p>
+          </div>
+        </div>
+
+        <div class="col-4">
+          <div class="border border-primary border-3 rounded m-3 p-3">
+            <i class="bi bi-apple text-primary"></i>
+            <p>Apple</p>
+          </div>
+        </div>
+      </div>
+      {/* contact section  */}
+      <div class="contact-bg">
+        <div class="row ">
+          <div class="col-6"></div>
+          <div class="col-6">
+            <div class="contact-box bg-info w-75 p-5 m-5">
+              <h3>Email</h3>
+              <h5>mailsohaghasan@gmail.com</h5>
+              <address>Address</address>
+              <p>Mohammadpur,Dhaka</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
+
   )
 }
 
 export default About
+
+
+
