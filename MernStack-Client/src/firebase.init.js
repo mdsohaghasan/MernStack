9e081b523d5802781ca840e1836705dad81bfe6c
Sohag Hasan <mailsohaghasan@gmail.com>
2022-05-11 23:35:02 +0600

    firebase connection update

--- MernStack-Client/src/firebase.init.js
+++ MernStack-Client/src/firebase.init.js
@@ -6,15 +6,17 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC6JC5i45WYVcUjHC7w1lKPnkvFzmHJaQc",
-  authDomain: "rana-auth.firebaseapp.com",
-  projectId: "rana-auth",
-  storageBucket: "rana-auth.appspot.com",
-  messagingSenderId: "376417782652",
-  appId: "1:376417782652:web:3b49a13bc4d9955ee4f2f9"
+  apiKey: "AIzaSyAJK5vSEjC0CxUEZ9RRTK3SPo5G-JLxRGQ",
+  authDomain: "warehouse-67541.firebaseapp.com",
+  projectId: "warehouse-67541",
+  storageBucket: "warehouse-67541.appspot.com",
+  messagingSenderId: "270314049492",
+  appId: "1:270314049492:web:28b428bad2d446e7eb5041"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 export default auth;
+
+
